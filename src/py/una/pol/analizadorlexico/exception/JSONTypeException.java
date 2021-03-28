a2bd5b453f6e3fb4b4edcaa53d99d8225e000313
Ana Isabel Jerez <isaux13@gmail.com>
2021-03-28 13:09:36 -0400

    Commit Inicial

---
+++ src/py/una/pol/analizadorlexico/exception/JSONTypeException.java
@@ -0,0 +1,8 @@
+package py.una.pol.analizadorlexico.exception;
+
+public class JSONTypeException extends RuntimeException {
+
+    public JSONTypeException(String message) {
+        super(message);
+    }
+}
---
+++ src/py/una/pol/analizadorlexico/tokenizer/CharReader.java
@@ -0,0 +1,61 @@
+package py.una.pol.analizadorlexico.tokenizer;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class CharReader {
+
+    private static final int BUFFER_SIZE = 1024;
+
+    private Reader reader;
+
+    private char[] buffer;
+
+    private int pos;
+
+    private int size;
+
+    public CharReader(Reader reader) {
+        this.reader = reader;
+        buffer = new char[BUFFER_SIZE];
+    }
+
+    public char peek() {
+        if (pos - 1 >= size) {
+            return (char) -1;
+        }
+
+        return buffer[Math.max(0, pos - 1)];
+    }
+
+    public char next() throws IOException {
+        if (!hasMore()) {
+            return (char) -1;
+        }
+
+        return buffer[pos++];
+    }
+
+    public void back() {
+        pos = Math.max(0, --pos);
+    }
+
+    public boolean hasMore() throws IOException {
+        if (pos < size) {
+            return true;
+        }
+
+        fillBuffer();
+        return pos < size;
+    }
+
+    void fillBuffer() throws IOException {
+        int n = reader.read(buffer);
+        if (n == -1) {
+            return;
+        }
+
+        pos = 0;
+        size = n;
+    }
+}
